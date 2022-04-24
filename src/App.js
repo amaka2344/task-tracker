ac08bdc6759d34652c0466a67a9d51e2a3ed15db
amaka <amibaby411@gmail.com>
2022-04-23 19:01:57 -0700

    your message here

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 //use state inside a function we use hook (USESTATE) 
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import About from './components/About';
 
 function App() {
-const [ShowAddTask, setShowAddTask] = useState(false)
+  const [ShowAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-     const getTasks = async () => {
-        const tasksFromServer = await fetchTasks()
-          setTasks(tasksFromServer)
-        
-     }
-  getTasks()
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+
+    }
+    getTasks()
   }, [])
 
+
+  //to fetch a single task to toggle reminder in the server
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks ${id}`)
+    const data = await res.json()
+    return data
+  }
 
   //fetch data
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
     const data = await res.json()
-   return data
+    return data
   }
 
-    //to fetch a single task to toggle reminder in the server
-    const fetchTask = async (id) => {
-      const res = await fetch(`http://localhost:5000/tasks ${id}`)
-      const data = await res.json()
-     return data
-    }
+
 
   //add task
   const addTask = async (task) => {
@@ -38,12 +43,12 @@ const [ShowAddTask, setShowAddTask] = useState(false)
       method: 'POST',
       headers: {
         'content-type': 'application/json',
-  },
-  body: JSON.stringify(task)
-})
+      },
+      body: JSON.stringify(task)
+    })
 
-  const data = await res.json()
-  setTasks([...tasks, data])
+    const data = await res.json()
+    setTasks([...tasks, data])
   }
 
   //delete task
@@ -58,7 +63,7 @@ const [ShowAddTask, setShowAddTask] = useState(false)
   //toggle reminder
   const toggleReminder = async (id) => {
     const taskToToggle = await fetchTask(id)
-    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder} 
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
     const res = await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'PUT',
@@ -71,24 +76,38 @@ const [ShowAddTask, setShowAddTask] = useState(false)
     const data = await res.json()
 
     setTasks(
-      tasks.map((task) => 
-      task.id === id ? { ...task, reminder:
-      !data.reminder } : task
-      ) 
+      tasks.map((task) =>
+        task.id === id ? {
+          ...task, reminder:
+            !data.reminder
+        } : task
+      )
     )
-   
+
   }
- 
+
 
   // adding of props and new compnents created
   return (
-    <div className="container">
-        
-      <Header onAdd = {()=> setShowAddTask(!ShowAddTask)} showAdd = {ShowAddTask}/>
-      {ShowAddTask && <AddTask onAdd = {addTask}/>}      
-      {tasks.length > 0 ? (
-      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ("No task to Show")}
-    </div>
+    <Router>
+      <div className="container">
+
+        <Header onAdd={() => setShowAddTask(!ShowAddTask)} showAdd={ShowAddTask} />
+
+       
+          <Route path='/' exact render={(props) => (
+            <>
+              {ShowAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />)
+                : ("No task to Show")}
+            </>
+          )}>
+          </Route>
+          <Route path='/About' components={About}></Route>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h4>version: 0.1.0</h4>
+      <a href="/">go back </a>
+    </div>
+  )
+}
+
+export default About
